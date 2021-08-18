388a00347cbdc9acb8536a792810406332063a28
joaofeiteira <70185564+joaofeiteira@users.noreply.github.com>
2021-08-17 22:26:42 -0300

    Aula 17/08

--- api/index.js
+++ api/index.js
@@ -1,5 +1,94 @@
+const { query } = require('express');
 const express = require('express')
 const servidor = express()
+const mysql = require("mysql2")
+const banco = mysql.createPool({
+    "database": "3j_2021",
+    "user": "root",
+    "password": "minas",
+    "host": "localhost",
+    "port": "3306"
+})
+
+
+
+servidor.get("/teste", (req, res, next) => {
+    banco.getConnection((error, conn) =>{
+        if (error) {
+            return res.status(500).send({
+                Erro: "Erro no servidor",
+                Detalhes: error,
+            });
+        }
+
+        conn.release();
+
+        return res.status(200).send ({
+            Mensagem: "Conexão estabelecida com sucesso",
+        });
+    });
+});
+
+
+servidor.get("/clientes", (req, res, next) => {
+    const QUERY = 'SELECT * FROM clientes ORDER BY cliente_id'
+
+    banco.getConnection((error, conn) => {
+        if(error){
+            return res.status(500).send({
+                Erro: "Não foi possível atender à solicitação",
+                Detalhes: error
+            })
+        }
+
+        conn.query(QUERY, (error, resultado) => {
+            conn.release()
+
+            if(error){
+                return res.status(500).send({
+                    Erro: "Não foi possível atender à solicitação",
+                    Detalhes: error
+                })
+            }
+
+            return res.status(200).send({
+                Mensagem: "Dados retornados com sucesso",
+                Dados: resultado
+            });
+        })
+    })
+})
+
+
+servidor.get("/clientes/:criterio", (req, res, next) => {
+    let criterio = req.params.criterio
+    const QUERY = `SELECT * FROM clientes WHERE nome like '%${criterio}%' ORDER BY nome`
+
+    banco.getConnection((error, conn) => {
+        if(error){
+            return res.status(500).send({
+                Erro: "Não foi possível atender à solicitação",
+                Detalhes: error
+            });
+        }
+
+        conn.query(QUERY, (error, resultado) => {
+            conn.release()
+
+            if(error){
+                return res.status(500).send({
+                    Erro: "Não foi possível atender à solicitação",
+                    Detalhes: error
+                });
+            }
+
+            return res.status(200).send({
+                Dados: resultado
+            })
+        })
+    })
+})
+
 
 servidor.get(
     '/', (req, res, next) => {
@@ -7,7 +96,7 @@ servidor.get(
             "mensagem": "Bem-vindo(a) ao servidor",
             "cidade": "Itapeva",
             "UF": "SP"
-        })
+        });
     }
 )
 
